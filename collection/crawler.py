d8959028e0d4d14bbcf880adba8bc2afad0fec1b
ynicekyhh <ynicekyhh@daum.net>
2020-08-14 15:38:48 +0900

    update

--- collection/crawler.py
+++ collection/crawler.py
@@ -6,16 +6,22 @@ import sys
 def crawling(
         url='',
         encoding='utf-8',
-        proc=lambda html: html,
-        store=lambda p: None,
+        proc=lambda h: h,
+        store=lambda d: d,
         err=lambda e: print('%s : %s' % (e, datetime.now()), file=sys.stderr)):
     try:
         request = Request(url)
         resp = urlopen(request)
 
-        html = resp.read().decode(encoding)
+        try:
+            receive = resp.read()
+            result = receive.decode(encoding)
+        except UnicodeDecodeError:
+            result = receive.decode(encoding, 'replace')
+
         print('%s : success for request [%s]' % (datetime.now(), url))
 
-        store(proc(html))
+        return store(proc(result))
+
     except Exception as e:
         err(e)
